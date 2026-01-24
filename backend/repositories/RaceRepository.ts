fbc50b192d8783a8de598aa1d2387d966eb91231
Décio Neto <decio.neeto@gmail.com>
2026-01-24 11:40:36 -0300

    finalmente fiz o prisma funcionar

--- backend/repositories/RaceRepository.ts
+++ backend/repositories/RaceRepository.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import prisma from "@/lib/prisma";
+import { prisma } from "@/lib/prisma";
 import { RaceData } from "../entities/Race";
 
 export async function fetchAllRaces(): Promise<RaceData[]> {
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,20 +1,6 @@
-import { PrismaClient } from "../app/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
-};
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
+export const prisma = new PrismaClient({
+  log: ["query"],
+  errorFormat: "pretty",
 });
-
-const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    adapter,
-  });
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-export default prisma;
